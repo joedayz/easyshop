9a42b2b8e7938873c19f589cf5601b1600ccb2e7
Jose Diaz <jose.diaz@joedayz.pe>
2025-03-18 19:26:27 -0500

    con maven

---
+++ backend/src/test/java/pe/joedayz/easyshop/EasyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.joedayz.easyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
