a41b002e6a5d4e1aa47b682e75c1a9215b971c40
Jose Diaz <jose.diaz@joedayz.pe>
2025-04-01 15:21:42 -0500

    Search bar

--- frontend/UI/src/components/Navigation/Navigation.jsx
+++ frontend/UI/src/components/Navigation/Navigation.jsx
@@ -16,8 +16,14 @@ const Navigation = () => {
                     <li><a href="/kids">Kids</a> </li>
                 </ul>
             </div>
-            <div>
+            <div className='flex justify-center'>
                 {/* Search bar */}
+                <div className='border rounded flex overflow-hidden'>
+                    <div className="flex items-center justify-center px-4 border-1">
+                        <svg className="h-4 w-4 text-grey-dark" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
+                        <input type="text" className="px-4 py-2 outline-none" placeholder="Search"/>
+                    </div>
+                </div>
             </div>
 
             <div>
