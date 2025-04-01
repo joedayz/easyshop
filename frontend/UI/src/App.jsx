7035031d30d4da3a85a18fcef93def4a8a70c422
Jose Diaz <jose.diaz@joedayz.pe>
2025-04-01 15:15:22 -0500

    navigation bar

--- frontend/UI/src/App.jsx
+++ frontend/UI/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navigation from "./components/Navigation/Navigation.jsx";
 
 function App() {
 
 
   return (
     <div className="App">
-      <h2 className='text-3xl font-bold underline'>Hello</h2>
+      <Navigation/>
     </div>
   )
 }
