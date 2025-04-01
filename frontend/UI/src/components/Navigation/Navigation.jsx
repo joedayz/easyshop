b26bec2e253ed47a0322d43883a3f558c114bbce
Jose Diaz <jose.diaz@joedayz.pe>
2025-04-01 15:25:38 -0500

    action items

--- frontend/UI/src/components/Navigation/Navigation.jsx
+++ frontend/UI/src/components/Navigation/Navigation.jsx
@@ -10,24 +10,33 @@ const Navigation = () => {
             <div className='flex flex-wrap items-center gap-8 flex-1'>
                 {/* Nav items */}
                 <ul className='flex gap-14 text-gray-600 hover:text-black'>
-                    <li><a href="/">Shop</a> </li>
-                    <li><a href="/mens">Men</a> </li>
-                    <li><a href="/womens">Women</a> </li>
-                    <li><a href="/kids">Kids</a> </li>
+                    <li><a href="/">Shop</a></li>
+                    <li><a href="/mens">Men</a></li>
+                    <li><a href="/womens">Women</a></li>
+                    <li><a href="/kids">Kids</a></li>
                 </ul>
             </div>
             <div className='flex justify-center'>
                 {/* Search bar */}
                 <div className='border rounded flex overflow-hidden'>
                     <div className="flex items-center justify-center px-4 border-1">
-                        <svg className="h-4 w-4 text-grey-dark" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
+                        <svg className="h-4 w-4 text-grey-dark" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                             viewBox="0 0 24 24">
+                            <path
+                                d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/>
+                        </svg>
                         <input type="text" className="px-4 py-2 outline-none" placeholder="Search"/>
                     </div>
                 </div>
             </div>
 
-            <div>
-                {/* Action items - icons */}
+            <div className='flex flex-wrap items-center gap-4'>
+                {/* Action Items - icons */}
+                <ul className='flex items-center gap-8 '>
+                    <li>Icon1</li>
+                    <li>Icon2</li>
+                    <li>Icon3</li>
+                </ul>
             </div>
         </nav>
     )
