ca0d76aa345ff52991e434684ce2298827fd72ff
Jose Diaz <jose.diaz@joedayz.pe>
2025-04-01 15:38:34 -0500

    Renombrar App a Shop

--- frontend/UI/src/Shop.jsx
+++ frontend/UI/src/Shop.jsx
@@ -1,7 +1,7 @@
-import './App.css'
+import './Shop.css'
 import Navigation from "./components/Navigation/Navigation.jsx";
 
-function App() {
+function Shop() {
 
 
   return (
@@ -11,4 +11,4 @@ function App() {
   )
 }
 
-export default App
+export default Shop
