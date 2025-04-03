8ecbe281b0dcd49a866699decaf73e6611730cb3
Jose Diaz <jose.diaz@joedayz.pe>
2025-04-03 16:43:12 -0500

    usando routing la primera vez

--- frontend/UI/src/main.jsx
+++ frontend/UI/src/main.jsx
@@ -3,9 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Shop from './Shop.jsx'
 import 'react-multi-carousel/lib/styles.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ProductListPage from "./pages/ProductListPage/ProductListPage.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Shop />,
+    },
+    {
+        path: "/womens",
+        element: <ProductListPage />,
+    }, // Ruta para 404
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Shop />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
