0f5448ed7b70c82a2a024ff5dde7559764cbe1c2
Jose Diaz <jose.diaz@joedayz.pe>
2025-04-03 16:28:58 -0500

    Footer

--- frontend/UI/src/Shop.jsx
+++ frontend/UI/src/Shop.jsx
@@ -4,6 +4,7 @@ import HeroSection from "./components/HeroSection/HeroSection.jsx";
 import NewArrivals from "./components/Sections/NewArrivals.jsx";
 import Category from "./components/Sections/Categories/Category.jsx";
 import content from "./data/content.json"
+import Footer from "./components/Footer/Footer.jsx";
 
 
 function Shop() {
@@ -15,6 +16,7 @@ function Shop() {
             <HeroSection/>
             <NewArrivals/>
             {content?.categories && content?.categories?.map((item, index) => <Category key={item?.title+index} {...item}/>)}
+            <Footer content={content?.footer}/>
         </div>
     )
 }
