882e7cb4ea28d8f004a49aa00a0e444a3a49948b
Jose Diaz <jose.diaz@joedayz.pe>
2025-04-03 16:00:43 -0500

    categories

--- frontend/UI/src/Shop.jsx
+++ frontend/UI/src/Shop.jsx
@@ -2,6 +2,9 @@ import './Shop.css'
 import Navigation from "./components/Navigation/Navigation.jsx";
 import HeroSection from "./components/HeroSection/HeroSection.jsx";
 import NewArrivals from "./components/Sections/NewArrivals.jsx";
+import Category from "./components/Sections/Categories/Category.jsx";
+import content from "./data/content.json"
+
 
 function Shop() {
 
@@ -11,6 +14,7 @@ function Shop() {
             <Navigation/>
             <HeroSection/>
             <NewArrivals/>
+            {content?.categories && content?.categories?.map((item, index) => <Category key={item?.title+index} {...item}/>)}
         </div>
     )
 }
--- frontend/UI/src/components/Card/Card.jsx
+++ frontend/UI/src/components/Card/Card.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import ArrowIcon from "../common/ArrowIcon.jsx";
 
 
-const Card = ({imagePath,title}) => {
+
+const Card = ({imagePath,title,description,actionArrow,height,width}) => {
     return (
-        <div className='flex  flex-col p-8'>
-            <img
-                className='max-h-[260px] max-w-[240px] min-h-[260px] min-w-[240px] bg-cover bg-center border rounded hover:scale-105 cursor-pointer'
-                src={imagePath} height={"320px"} width={"420px"} alt='Jeans'/>
-            <p className='text-[16px] pt-[5px]'>{title}</p>
+        <div className='flex  flex-col p-6'>
+            <img className={`h-[${height? height:'220px'}] max-h-[${height? height:'220px'}] w-[${width? width:'200px'}] max-w-[${width? width:'220px'}]
+         border rounded-lg hover:scale-105 cursor-pointer`} width={width??"200px"} height={height?? "220px"} src={imagePath} alt='Jeans'/>
+            <div className='flex justify-between items-center'>
+                <div className='flex flex-col'>
+                    <p className='text-[16px] p-1'>{title}</p>
+                    {description && <p className='text-[12px] px-1 text-gray-600'>{description}</p>}
+                </div>
+                {actionArrow && <span className='cursor-pointer pr-2 items-center'><ArrowIcon /></span>}
+            </div>
         </div>
     )
 }
