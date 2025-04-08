8f1c2cfe344deda9f1b0823f9b3967b0c904e53c
Jose Diaz <jose.diaz@joedayz.pe>
2025-04-08 10:19:31 -0500

    kids

--- frontend/UI/src/main.jsx
+++ frontend/UI/src/main.jsx
@@ -25,6 +25,10 @@ const router = createBrowserRouter([
                 path: "/men",
                 element: <ProductListPage categoryType={'MEN'}/>,
             },
+            {
+                path: "/kids",
+                element: <ProductListPage categoryType={'KIDS'}/>,
+            },
         ]
     }
 ]);
