9702e20099829b53148f8714bed48fd0976f4358
Jose Diaz <jose.diaz@joedayz.pe>
2025-04-08 18:29:23 -0500

    breadcrumb

--- frontend/UI/src/pages/ProductDetailPage/ProductDetails.jsx
+++ frontend/UI/src/pages/ProductDetailPage/ProductDetails.jsx
@@ -1,10 +1,37 @@
-import React, {useState} from 'react'
+import React, {useEffect, useMemo, useState} from 'react'
 import {useLoaderData} from "react-router-dom";
+import Breadcrumb from "../../components/Breadcrumb/Breadcrumb.jsx";
+import content from '../../data/content.json'
+
+
+const categories = content?.categories;
 
 const ProductDetails = () => {
 
     const { product } = useLoaderData();
     const [image, setImage] = useState(product?.images[0]?.startsWith('http') ? product?.images[0] : product?.thumbnail);
+    const [breadCrumbLinks, setBreadCrumbLink] = useState([]);
+
+    const productCategory = useMemo(()=>{
+       return categories?.find((category)=>category?.id === product?.category_id);
+    },[product]);
+
+    useEffect(() => {
+        setBreadCrumbLink([]);
+        const arrayLinks = [{ title: 'Shop', path:'/'}, {
+            title: productCategory?.name,
+            path: productCategory?.path
+        }];
+        const productType = productCategory?.types?.find((item)=> item?.type_id === product?.type_id);
+        if(productType){
+            breadCrumbLinks?.push({
+              title: productType?.name,
+              path: productType?.path
+            })
+        }
+        setBreadCrumbLink(arrayLinks);
+    },[productCategory, product])
+
 
     return (
         <div className='flex flex-col md:flex-row px-10'>
@@ -15,8 +42,8 @@ const ProductDetails = () => {
                         {/* Stack images */}
                         <div className='flex flex-row md:flex-col justify-center h-full'>
                             {
-                                product?.images?.map((item, index) => (
-                                    <button key={index} onClick={() => setImage(item?.url)} className='rounded-lg w-fit p-2 mb-2'>
+                                product?.images[0]?.startsWith('http') && product?.images?.map((item, index) => (
+                                    <button key={index} onClick={() => setImage(item)} className='rounded-lg w-fit p-2 mb-2'>
                                         <img src={item} className='h-[60px] w-[60px] rounded-lg bg-cover bg-center hover:scale-105 hover:border'
                                              alt={'sample-' + index} /></button>
                                 ))
@@ -30,6 +57,12 @@ const ProductDetails = () => {
                     </div>
                 </div>
             </div>
+            <div className='w-[60%]'>
+                {/* Product Description */}
+                <Breadcrumb links={breadCrumbLinks}/>
+                <p className='text-3xl pt-2'>{product?.title}</p>
+            </div>
+
         </div>
     )
 }
