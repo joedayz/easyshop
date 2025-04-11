cec3a0a9cc5d4630a3f8c5baec6645fad096d418
Jose Amadeo Diaz <jamdiazdiaz@gmail.com>
2025-04-10 20:51:26 -0500

    usando loader con react router dom

--- frontend/UI/src/pages/ProductDetailPage/ProductDetails.jsx
+++ frontend/UI/src/pages/ProductDetailPage/ProductDetails.jsx
@@ -4,9 +4,9 @@ import {useLoaderData} from "react-router-dom";
 const ProductDetails = () => {
 
     const { product } = useLoaderData();
-    //const [image, setImage] = useState(product?.images[0]?.startsWith('http') ? product?.image[0] : product?.thumbnail);
 
     return (
+
         <div>ProductDetails { product?.title }</div>
     )
 }
